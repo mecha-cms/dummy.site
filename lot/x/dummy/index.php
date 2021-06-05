c83e287306c4c88425f524d96d78dfe8133a0c50
Taufik Nurrohman <hi@taufik-nurrohman.com>
2021-06-05 10:12:33 +0700

    Add Unicode Font(s) Example

--- lot/x/dummy/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Hook::set('page.content', function($content) use($url) {
-    return strtr($content, [
-        ' href="/' => ' href="' . $url . '/',
-        ' src="/' => ' src="' . $url . '/'
-    ]);
-});
